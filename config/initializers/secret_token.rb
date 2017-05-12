e3d54de8fdd228b48c306c299da717cd9c740d38
hemantmundra <hemant.mundra@housing.com>
2017-05-12 15:46:48 +0530

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HousingBid::Application.config.secret_token = '534889a2938b8816af07439dc65318df691e2c973e3dbe6879a93872aa3acc76661b4ea22837132e2ae81bb69972607e5dcd08a9994151e49fc2ee60a907106d'
